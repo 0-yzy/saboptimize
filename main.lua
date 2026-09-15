15f77b8e4db930b30361c42479312f24fc39aed7
yzy <bastian0z@outlook.com>
2026-09-15 22:57:23 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,40 @@
+function optimize(p)
+    if p:IsA("BasePart") then
+        p.Material = Enum.Material.Plastic
+    elseif p:IsA("Decal") or p:IsA("SurfaceAppearance") or p:IsA("MaterialVariant") or p:IsA("ParticleEmitter") or p:IsA("Pants") or p:IsA("Shirt") or p:IsA("Accessory") then
+        p:Destroy()
+    end
+    if p:IsA("MeshPart") then
+        p.TextureID = ""
+    elseif p:IsA("SpecialMesh") then
+        p.TextureId = ""
+    end
+end
+task.spawn(function()
+    while true do
+        for _,p in game:GetDescendants() do
+            optimize(p)
+        end
+        task.wait(10)
+    end
+end)
+task.spawn(function()
+    while true do
+        for _,p in workspace:GetDescendants() do
+            optimize(p)
+        end
+        task.wait(1)
+    end
+end)
+local l = game.Lighting
+l.Ambient = Color3.fromRGB(255, 255, 255)
+l.Brightness = 0
+l.ColorShift_Bottom = Color3.fromRGB(0, 0, 0)
+l.ColorShift_Top = Color3.fromRGB(0, 0, 0)
+l.EnvironmentDiffuseScale = 0
+l.EnvironmentSpecularScale = 0
+l.GlobalShadows = false
+l.Technology = Enum.Technology.Compatibility
+for i,v in l:GetChildren() do
+    v:Destroy()
+end
